5192403fd3656cf2a98049b16a4c0c62b495361c
haowei93 <727171008@qq.com>
2024-03-26 17:09:17 +0800

    add yihong0618

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,11 +11,11 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https:/',
+  siteUrl: 'https://github.com/yihong0618/running_page',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
-    
+
   ],
 };
 
