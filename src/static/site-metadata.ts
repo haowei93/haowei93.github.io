706547d507ba619a3001ae8893bebfc1a43743a2
haowei93 <727171008@qq.com>
2025-04-27 17:54:35 +0800

    upd summary

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -20,7 +20,10 @@ const data: ISiteMetadataResult = {
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
-
+    {
+      name: 'Summary',
+      url: `${getBasePath()}/summary`,
+    }
   ],
 };
 
